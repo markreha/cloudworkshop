46f03d5b3f47125c75f35e99de72ba8f1ead3401
Mark Reha <markreha@mac.com>
2017-04-15 21:30:08 +0000

    Added some comments

--- LED.py
+++ LED.py
@@ -1,16 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
+# Initialize GPIO for Pin 17
 print("Starting LED and GPIO test...........")
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
+
+# Turn LED On
 GPIO.output(17,GPIO.HIGH)
 print("LED on")
 time.sleep(5)
+
+# Turn LED Off
 GPIO.output(17,GPIO.LOW)
 print("LED off")
 time.sleep(5)
+
+# Turn LED On
 GPIO.output(17,GPIO.HIGH)
 print("LED on")
 print("Completed LED and GPIO test")
