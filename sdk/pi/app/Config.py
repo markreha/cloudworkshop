a8fc9e516dc6553c70477c4ae55f6bb589a6a385
Mark Reha <markreha@mac.com>
2017-05-08 23:31:39 +0000

    Updated config

--- sdk/pi/app/Config.py
+++ sdk/pi/app/Config.py
@@ -11,15 +11,15 @@ debug = True
 deviceID = 0
 
 # Different environment configurations
-env_dev1 = {'webApi': 'http://marks-macbookair.local:8080/cloudservices/rest/dht11/save',
+env_dev1 = {'webApi': 'http://marks-macbookair.local:8080/cloudservices/rest/weather/save',
          	'username': 'CloudWorkshop',
          	'password': 'dGVzdHRlc3Q='}
-env_dev2 = {'webApi': 'http://node5.codenvy.io:37551/cloudservices/rest/dht11/save',
+env_dev2 = {'webApi': 'http://node5.codenvy.io:37551/cloudservices/rest/weather/save',
          	'username': 'CloudWorkshop',
          	'password': 'dGVzdHRlc3Q='}
-env_qa = {'webApi': 'http://cloud-services-playlaravel.44fs.preview.openshiftapps.com/cloadservices/rest/dht11/save',
+env_qa = {'webApi': 'http://cloudservices-workshop.1d35.starter-us-east-1.openshiftapps.com/cloadservices/rest/weather/save',
          	'username': 'CloudWorkshop',
          	'password': 'dGVzdHRlc3Q='}
-env_prod = {'webApi': 'http://cloud-services-playlaravel.44fs.preview.openshiftapps.com/cloadservices/rest/dht11/save',
+env_prod = {'webApi': 'http://cloudservices-workshop.1d35.starter-us-east-1.openshiftapps.com/cloadservices/rest/weather/save',
          	'username': 'CloudWorkshop',
          	'password': 'dGVzdHRlc3Q='}
