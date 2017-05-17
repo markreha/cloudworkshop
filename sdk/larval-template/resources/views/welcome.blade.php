3676eecb88507b3073f436e061490e2198c2e984
Mark Reha <mark@Marks-MacBook-Pro.local>
2017-05-17 12:42:53 -0700

    First time check in.

---
+++ sdk/larval-template/resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+    	<!-- Metadata Tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Page Title -->
+        <title>GCU Cloud Workshop</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="resources/assets/css/weather.css" rel="stylesheet" type="text/css" >
+    </head>
+	<body>
+        <div class="flex-center position-ref full-height">
+        	<!-- Top Menu -->
+            <div class="top-right links">
+                <a href="https://github.com/markreha/cloudworkshop/tree/master/sdk/docs" target="_blank">SDK Documentation</a>
+                <a href="https://github.com/markreha/cloudworkshop/tree/master/sdk" target="_blank">GitHub</a>
+                <img src="resources/assets/image/logo.png" width="65" height="65"/>
+			</div>
+ 
+ 			<!-- Page Content -->
+            <div class="content">
+            	<!--  Title and Menu Links -->
+                <div class="title m-b-md">
+                    GCU IoT Cloud Workshop
+                </div>
+                <div class="links">
+                    <a href="weather">Weather</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
