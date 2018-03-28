1165b8c41b0edebe88bde73033a1560a1d82c745
Mark Reha <markreha@mac.com>
2018-03-28 07:26:55 -0700

    Update IoT.sql

--- sdk/docs/database/IoT.sql
+++ sdk/docs/database/IoT.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `iot` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `iot`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: iot
